7b42762ed4017965dc39bfe18115e56887c9a2a0
Nick Hayward <nhayward2011@gmail.com>
2025-08-12 10:33:59 -0400

    setting up the board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { useState } from 'react';
-
-function Square() {
-  const [value, setValue] = useState(null);
-
+function Square({ value }) {
   function handleClick() {
-    setValue('X');
+    console.log('clicked!');
   }
 
   return (
@@ -21,19 +17,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
